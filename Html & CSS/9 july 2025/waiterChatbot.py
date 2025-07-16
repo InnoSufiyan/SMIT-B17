b2e8151d27330ae15f84347ad0d3f05da2525899
InnoSufiyan <innosufiyan@gmail.com>
2025-07-16 22:50:45 +0500

    deleted .py files

--- Html & CSS/9 july 2025/waiterChatbot.py
+++
@@ -1,9 +0,0 @@
-food=input("what would you like to eat")
-if food=="Lasagna":
-    print("which type of lasagna?")
-elif food=="Pizza":
-    print("would you like topping as well?")
-elif food=="Biryani":
-    print("will it be chicken biryani or mutton biryani?")
-else:
-    print("Okay sir I am will be in 5 mints with you order ",food)
